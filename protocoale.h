2113991a0018098be876bcd12278a47454717147
Emanuel-Tarcisiu <127722707+Emanuel-Tarcisiu@users.noreply.github.com>
2023-04-25 22:37:43 +0300

    Update protocoale.h

--- protocoale.h
+++ protocoale.h
@@ -2,5 +2,11 @@
 #include<string>
 using namespace std;
 
+//PROT LEGATE DE CONTURI
 #define PROT_LOG_IN string("log_in_instr")
 #define PROT_REGISTER string("register_instr")
+
+//PROT LEGATE DE TIPUL DE CONT CARE S-A LOGAT
+#define PROT_LOG_IN_STUDENT string("log_in_stud")
+#define PROT_LOG_IN_PROFESOR string("log_in_prof")
+#define PROT_LOG_IN_ADMIN string("log_in_admin")
